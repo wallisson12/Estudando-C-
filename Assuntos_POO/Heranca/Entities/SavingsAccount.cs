a3a5ed7a9ad616aa77b932aa5a223b1c27c4c7d2
wallisson12 <wallisson_wjc@outlook.com.br>
2022-02-28 12:52:19 -0300

    Casting

---
+++ Assuntos_POO/Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        //O "base" serve para referenciar a superClasse assim utilizando o construtor de "Account",assim fazendo a reutilização de codigo.
+        public SavingsAccount(int number, string holder, double balance,double interestRate) : base(number,holder,balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        //Metodos
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
--- Assuntos_POO/Heranca/Program.cs
+++ Assuntos_POO/Heranca/Program.cs
@@ -7,12 +7,43 @@ namespace Heranca
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "wallisson", 100.0, 500.0);
 
-            Console.WriteLine(account.Balance);
+            Account acc = new Account(1001, "wallisson",0.0);
+            BusinessAccount bcc = new BusinessAccount(1002, "Alex", 0.0, 500.0);
 
-            //Vai dar erro pois ela não é permitida para outras classes(Protected)
-            //account.Balance = 30.0;
+            //UpCasting
+
+            //Isso é possivel pq a hereança é uma relação "é um", ou seja o "bcc" é uma "Account".
+            Account acc2 = bcc;
+
+            //Na variavel do tipo "Account" tem um OBJ do tipo "BusinessAccount".
+            Account acc3 = new BusinessAccount(1003,"Bob",0.0,200.0);
+            Account acc4 = new SavingsAccount(1004,"Anna",0.0,0.01);
+
+            //DownCasting
+
+            //Possso fazer o casting utilizando o "as"
+            BusinessAccount acc5 = acc3 as BusinessAccount;
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+            acc5.Loan(100.0);
+
+           // BusinessAccount acc7 = (BusinessAccount)acc4;
+
+            //Verifico se o acc4 é uma instancia de BusinessAccount
+            if (acc4 is BusinessAccount)
+            {
+                BusinessAccount acc6 = (BusinessAccount)acc4;
+                acc6.Loan(200.0);
+                Console.WriteLine("Loan!");
+
+            }
+
+            if (acc4 is SavingsAccount)
+            {
+                SavingsAccount acc7 = (SavingsAccount)acc4;
+                acc7.UpdateBalance();
+                Console.WriteLine("UpDate");
+            }
         }
     }
 }

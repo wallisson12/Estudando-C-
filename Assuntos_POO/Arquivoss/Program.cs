f8221fe84a8cb1796ba330bfad3ced58f4916bc6
Wallisson De Jesus <wallisson_wjc@outlook.com.br>
2022-09-27 11:46:22 -0300

    File e FileInfo

--- Assuntos_POO/Arquivoss/Program.cs
+++ Assuntos_POO/Arquivoss/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Arquivoss
 {
@@ -6,7 +7,33 @@ namespace Arquivoss
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Utiliza o @ para não precisar utilizar duas barras para o caminho do arquivo
+            string sourcePath = @"c:\Teste\Texto1.txt";
+
+            string targetPath = @"c:\Teste\Texto2.txt";
+
+            try
+            {
+                //Precisa de um caminho como parametro
+                //Instanciando um objeto que vai estar associado a um arquivo em um determinado caminho
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                fileInfo.CopyTo(targetPath);
+
+                //Vai abir o arquivo nesse caminho e ler todas as linhas e colocalas no vetor
+                //Cada linha vai ser um elemento do vetor
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines )
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
         }
     }
 }

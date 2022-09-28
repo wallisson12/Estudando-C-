d09590bc07da3ac9966d760492e41f7ff6a64eb4
Wallisson De Jesus <wallisson_wjc@outlook.com.br>
2022-09-28 13:52:25 -0300

    Operacoes com directory

---
+++ Assuntos_POO/Arquivoss4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Arquivoss4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = @"c:\Teste";
+
+            try
+            {
+                //Colecao do c#
+                //Lista as pastas no entanto pode listar os arquivos tbm
+               IEnumerable<string> folder = Directory.EnumerateDirectories(Path,"*.*",SearchOption.AllDirectories);
+                Console.WriteLine("Folders: ");
+                foreach (string s in folder)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //Criando uma pasta
+                Directory.CreateDirectory(Path + "\\newFolder");
+
+            }catch(IOException e)
+            {
+                Console.WriteLine("Erorr: " + e.Message);
+            }
+        }
+    }
+}

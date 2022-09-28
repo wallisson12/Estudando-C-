5d779d4f728b8bfccf89ba19c762b155e048f4fe
Wallisson De Jesus <wallisson_wjc@outlook.com.br>
2022-09-28 13:08:05 -0300

    StreamWriter

---
+++ Assuntos_POO/Arquivoss3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace Arquivoss3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\Teste\Texto1.txt";
+            string targetPath = @"c:\Teste\Texto2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}

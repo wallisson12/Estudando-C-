57bec39d0812784eb2fac4ef5efaee5855621786
Wallisson De Jesus <wallisson_wjc@outlook.com.br>
2022-10-03 11:19:16 -0300

    Estudando a classe Path

---
+++ Assuntos_POO/Arquivo5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A classe Path faz operacoes com caminhos
+
+            string path = @"c:\Teste\SubPasta\aaa.txt";
+
+            //Operacoes com path
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+        }
+    }
+}

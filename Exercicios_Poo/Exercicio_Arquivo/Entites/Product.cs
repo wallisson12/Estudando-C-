8b6d3ee2ec0eee6651630b026d3aa2df3ef490c4
Wallisson De Jesus <wallisson_wjc@outlook.com.br>
2022-10-03 13:14:33 -0300

    Exercicio sobre arquivo

---
+++ Exercicios_Poo/Exercicio_Arquivo/Entites/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Schema;
+
+namespace Exercicio_Arquivo.Entites
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int Amount { get; private set; }
+
+        public Product(string name,double price,int amount)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+
+        public double Total()
+        {
+            return Amount * Price;
+        }
+
+    }
+}
---
+++ Exercicios_Poo/Exercicio_Arquivo/Program.cs
@@ -0,0 +1,56 @@
+﻿using Exercicio_Arquivo.Entites;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Exercicio_Arquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> product = new List<Product>();
+
+            string sourcePath = @"c:\Teste\source.txt";
+            string targetPath = @"c:\Teste\out\out2.txt";
+
+
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+
+                    foreach (string line in lines)
+                    {
+                        string[] l = line.Split(',');
+
+                        string name = l[0];
+                        double price = double.Parse(l[1],CultureInfo.InvariantCulture);
+                        int amount = int.Parse(l[2]);
+
+                        product.Add(new Product(name, price, amount));
+
+                    }
+
+                    foreach (Product p in product)
+                    {
+                        sw.WriteLine(p.Name + "," + p.Total().ToString("F2",CultureInfo.InvariantCulture));
+                    }
+
+                }
+                
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+        }
+    }
+}
